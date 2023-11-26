0a4d57a20711ee120cf9d9bbcfe22c700baee0a8
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2023-11-25 18:59:32 -0600

    Utilize new Gemwork dependencies + test support
    This simplifies definition and update of gem dependencies and
    utilization of test/test_helper.rb support/requires.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,24 +1,14 @@
 # frozen_string_literal: true
 
-require "simplecov"
-SimpleCov.start do
-  add_filter "/bin/"
-  add_filter "/test/"
-end
-puts "SimpleCov enabled."
+require "gemwork/test/support/simplecov"
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "object_inspector"
 
 require "minitest/autorun"
-require "minitest/reporters"
+
+require "gemwork/test/support/reporters"
+require "gemwork/test/support/much_stub"
+require "gemwork/test/support/spec_dsl"
+
 require "pry"
-
-Minitest::Test.make_my_diffs_pretty!
-reporter_options = { color: true }
-Minitest::Reporters.use!(
-  Minitest::Reporters::DefaultReporter.new(reporter_options))
-
-def context(...)
-  describe(...)
-end
