40f4abd6264757688a8b55df0bf214af3eeee1f1
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-01-04 10:22:30 -0600

    Bump to v0.8.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## [Unreleased]
 
+### 0.8.0 - 2025-1-4
+
 - Update minimum Ruby version from 3.1 -> 3.2
 
 ### 0.7.0 - 2024-11-21
--- lib/object_inspector/version.rb
+++ lib/object_inspector/version.rb
@@ -2,5 +2,5 @@
 
 module ObjectInspector
   # The current ObjectInspector gem version.
-  VERSION = "0.7.0"
+  VERSION = "0.8.0"
 end
